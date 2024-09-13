203b0417bf6209d1fe7c86bf3278a68d23d53db1
eelias13 <maierelias13@gmai.com>
2024-09-13 15:32:04 +0200

    redo build process

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-python3 script.py release
-cd build/
-python3 -m http.server 4000
+cd rust && wasm-pack build --target no-modules --no-typescript --release && cd ..
+mkdir build
+cp -r vs build/vs
+
+minifier -o build/script.js web/script.js
+minifier -o build/rust.js rust/pkg/rust.js
+
+cp web/index.html build/index.html
+cp rust/pkg/rust_bg.wasm build/rust_bg.wasm
